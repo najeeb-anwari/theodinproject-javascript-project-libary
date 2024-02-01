093a392bd4f7c32c0bf99b8c5432486db77355dc
Najeeb Anwari <najeeb.anwari@gmail.com>
2024-02-01 15:11:54 +0430

    Added Poppins font and Read or Unread button for book

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,8 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+
+
 const booksContainer = document.querySelector(".books-container");
 
 const addToBookBtn = document.querySelector(".open-dialog-btn");
@@ -20,13 +22,58 @@ closeDialogBtn.addEventListener("click", (e) => {
 });
 
 const bookForm = document.querySelector("#bookForm");
-
+let bookIdCounter = 1;
 bookForm.addEventListener("submit", (e) => {
-    const bookName = document.querySelector("#name").value;
+    
+    const bookName = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
     let book = new Book(bookName, author, pages);
+    book.id = bookIdCounter++;
     addBookToLibrary(book);
-    console.log(myLibrary);
+    booksContainer.innerHTML = ``;
+    myLibrary.forEach(book => {
+        const bookCard = `
+        <div class="card book-item">
+            <div class="card-content">
+                
+                <h2>Title: ${book.title}</h2>
+                
+                <div class="book-field">
+                    <label for="author">Author:</label>
+                    <span>${book.author}</span>
+                </div>
+
+                <div class="book-field">
+                    <label for="pages">Pages:</label>
+                    <span>${book.pages}</span>
+                </div>
+
+                <div class="book-field">
+                    <label for="pages">Status:</label>
+                    <span class="book-status">${book.read ? "Read" : "Not Read Yet"}</span>
+                </div>
+                    
+                <hr/>
+                <button class="btn btn-sm btn-primary readToggle" data-id="${book.id}">${book.read ? "Mark as Unread" : "Mark as Read" }</button>
+                    
+            </div>
+        </div>
+        `;
+        booksContainer.innerHTML += bookCard;
+    });
+    const readToggleBtns = document.querySelectorAll(".readToggle");
+    readToggleBtns.forEach((readToggleBtn) => {
+        readToggleBtn.addEventListener("click", (f) => {
+            const id = f.target.getAttribute("data-id");
+            const book = myLibrary.filter(b => b.id == id)[0];
+            book.toggleRead();
+            const bookStatus = book.read ? "Read" : "Not Read Yet";
+            readToggleBtn.innerHTML = book.read ? "Mark as Unread" : "Mark as Read";
+            const bookStatusElement = f.target.parentNode.querySelector(".book-status");
+            bookStatusElement.innerHTML = bookStatus;
+        });
+    });
 });
+
 
